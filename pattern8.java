be7a7b0cb33e00c102eadf4caedd391061ae4fed
sumitdesai9500 <sumitdesai9500@gmail.com>
2023-06-25 20:29:06 +0530

    floyd's triangle

---
+++ pattern8.java
@@ -0,0 +1,13 @@
+//floyd's triangle
+public class pattern8 {
+    public static void main(String args[]) {
+        int n = 1;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(n + " ");
+                n++;
+            }
+            System.out.println();
+        }
+    }
+}
